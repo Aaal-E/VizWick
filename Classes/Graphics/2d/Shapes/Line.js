f6dedb722b920135e0f242d08276e63114e1e5a2
Mara Miulescu <34277789+maramiulescu@users.noreply.github.com>
2018-05-08 11:54:56 +0200

    Added line shape for connecting nodes
    We could use Rectangle instead of Line, if it turns out that this class does not work properly

---
+++ Classes/Graphics/2d/Shapes/Line.js
@@ -0,0 +1,57 @@
+/*
+    Author: Mara Miulescu
+    Date:   08/05/2018
+*/
+
+    class Line2d extends Shape2d {
+         constructor(graphics, startX, startY, endX, endY, width, color) {
+           super(graphics, color);
+           this.setWidth(width);
+           this.setStartPoints(startx, starty);
+           this.setEndPoints(endx, endy);
+         }
+
+         __redraw() {
+           this.gfx.clear();
+           this.gfx.beginFill(this.color);
+           this.gfx.moveTo(startX, startY);
+           this.gfx.lineTo(endX, endY);
+           this.gfx.endFill();
+         }
+
+         setWidth(width) {
+           this.width = width;
+           this.__redraw();
+           return this;
+         }
+
+         setStartPoints(startX, startY) {
+           this.startX = startX;
+           this.startY = startY;
+           return this;
+         }
+
+         setEndPoints(endX, endY) {
+           this.endX = endX;
+           this.endY = endY;
+           return this;
+         }
+
+         getWidth() {
+           return this.width;
+         }
+
+         getStartPoints() {
+           return {
+             startX: this.startX,
+             startY: this.startY;
+           };
+         }
+
+         getEndPoints() {
+           return {
+             endX: this.endX;
+             endY: this.endY;
+           };
+         }
+    }
