3e7b7dceff3c0ae05260256dbcabc9bd3e015746
Mara Miulescu <34277789+maramiulescu@users.noreply.github.com>
2018-05-08 11:51:57 +0200

    Update Polygon.js

--- Classes/Graphics/2d/Shapes/Polygon.js
+++ Classes/Graphics/2d/Shapes/Polygon.js
@@ -4,15 +4,33 @@
 */
 
     class Polygon2d extends Shape2d {
-        constructor(graphics, points[], color) {
+        constructor(graphics, points, color) {
           super(graphics, color);
-          // takes as argument an array of points ([x,y, x,y, x, y,..])
-          this.setPoints(points[]);
+          this.setPoints(points);
         }
 
         __redraw() {
           this.gfx.clear();
           this.gfx.beginFill(this.color);
-          this.gfx.drawPolygon(0, 0, this.)
+          this.gfx.drawPolygon(this.points);
+          this.gfx.endFill();
+
+          this.gfx.hitArea = new PIXI.Polygon(points);
+        }
+
+
+        // takes as argument an array of points ([x1,y1, x2,y2, x3, y3,..])
+        setPoints(points) {
+          this.points = points;
+          this.__redraw();
+          return this;
+        }
+
+        getPoints() {
+          return this.points;
+        }
+
+        __getPoints() {
+          return this.points;
         }
     }
