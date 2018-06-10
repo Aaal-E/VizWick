e4ec11f259745956ed9ade72aac8dcf7fe3e450f
denisshehu <38808126+denisshehu@users.noreply.github.com>
2018-06-10 10:19:54 +0200

    The option pane collapses and reappears (and some minor changes)

--- DBL Website/Resources/JavaScript/Visualization page.js
+++ DBL Website/Resources/JavaScript/Visualization page.js
@@ -3,6 +3,10 @@
 
 $(function(){
 
+  //Resizing of the visualization areas
+  $(".layout").resizeContainer({vertical:true});
+  $(".top-layout-part, .bottom-layout-part").resizeContainer({vertical:false});
+
   //Collapsing and appearing the information section
   $(".collapse").click(function(){
     $(".visualization-page").attr("id", "information-collapse");
@@ -20,12 +24,36 @@ $(function(){
   });
 
   //Collapsing and appearing the options
-  $(".top-layout-part").click(function(){
-    console.log("A");
+  //top-left visualization area
+  $(".top-left .lower-arrow").click(function(){
+    $(".top-left .option-pane").attr("id", "options");
+  });
+  $(".top-left .upper-arrow").click(function(){
+    $(".top-left .option-pane").attr("id", "notch");
   });
 
-  //Resizing of the visualization areas
-  $(".layout").resizeContainer({vertical:true});
-  $(".top-layout-part, .bottom-layout-part").resizeContainer({vertical:false});
+  //top-right visualization area
+  $(".top-right .lower-arrow").click(function(){
+    $(".top-right .option-pane").attr("id", "options");
+  });
+  $(".top-right .upper-arrow").click(function(){
+    $(".top-right .option-pane").attr("id", "notch");
+  });
+
+  //bottom-left visualization area
+  $(".bottom-left .upper-arrow").click(function(){
+    $(".bottom-left .option-pane").attr("id", "options");
+  });
+  $(".bottom-left .lower-arrow").click(function(){
+    $(".bottom-left .option-pane").attr("id", "notch");
+  });
+
+  //bottom-right visualization area
+  $(".bottom-right .upper-arrow").click(function(){
+    $(".bottom-right .option-pane").attr("id", "options");
+  });
+  $(".bottom-right .lower-arrow").click(function(){
+    $(".bottom-right .option-pane").attr("id", "notch");
+  });
 
 });
