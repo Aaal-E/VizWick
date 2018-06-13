1bee6dff9c79cf91b8500e927ea87281d8abb0cf
TarVK <tarvankrieken@gmail.com>
2018-06-13 23:59:03 +0200

    Moved Alina's code, cleaned it up a little, changed to slide transition

---
+++ Src/Webpage/PreviewPage.js
@@ -0,0 +1,21 @@
+$(function(){
+  $(".fa-chevron-right").click(function(){
+    selectIndex((selectedIndex+1)%color.length);
+  });
+
+  $(".fa-chevron-left").click(function(){
+    selectIndex((selectedIndex-1+color.length)%color.length);
+  });
+
+  $(".dot").click(function(){
+    counter=$(this).attr('id');
+    selectIndex($(this).attr('id'));
+  });
+
+});
+var color=["red", "black","gray", "yellow", "blue"];
+var selectedIndex = 0;
+function selectIndex(index){
+  selectedIndex = index;
+  $('.photo-area').css('background-color', color[index]);
+}
