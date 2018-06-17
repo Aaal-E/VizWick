4297c9ac487aa569809403c28b2cf22dce4196a7
Mehrdad <mh.farsadyar@gmail.com>
2018-06-17 17:37:37 +0200

    The updated Optimized Sunburst and Radial Band
    The updated Optimized Sunburst and Radial Band

--- Tests/Visualisation/Scripts/OptimizedSunburst.js
+++ Tests/Visualisation/Scripts/OptimizedSunburst.js
@@ -2,8 +2,9 @@
     Optimized Sunburst Visualisation
     Authors:
         Mehrdad Farsadyar
+        Tar van Krieken
         Mara Miulescu
-        
+
     Starting Date: 25/05/2018
 */
 
@@ -17,8 +18,8 @@
     class NodeShape extends VIZ2D.NodeShape{
         //
         constructor(gfx, node){
-            super(gfx, node); //We forward the it in the constructor, in order to save it as an object field
-            //A nodeshape doesn't render anything, so we must add shapes to it to display
+            super(gfx, node);
+            //A nodeshape doesn't render anything; must add shapes to it to display
             this.radialBand = new VIZ2D.RadialBand(gfx, 0, 25, this.calcRadian(0), this.calcRadian(360),
                 0xff0000);
             this.addShape(this.radialBand);
@@ -29,12 +30,6 @@
             });
         }
 
-        //In case in future setting location of shapes becomes an issue
-        // __connectParent(parent){
-        //     if(parent){ //doesn't have a parent if root
-        //     }
-        // }
-
         //
         __stateChanged(field, val, oldState){
             if(field=="focused" && val==true){
@@ -42,34 +37,23 @@
             }
         }
 
-        //calcs PDCount (= Partial Descendant Count) for this node and all its descendants.
-        //The count of a node includes the node itself
-        //The method automathically stops when there are no more nodes available,
-        //so passing layer is not necessary
-        calcPDCount(){
+        /*
+        calcs PDCount (= Partial Descendant Count) for this node and all its
+        descendants. It also sets the layer number.
+        The count of a node includes the node itself. The method automathically
+        stops when there are no more nodes available,so passing layer is not necessary
+        */
+        calcPDCount_setLayersNumbers(){
             this.PDCount = 1;
             var childArr = this.getChildren();
+            var lhca = this.getVisualisation().layerHeadCountArr;
             for(var i=0; i < childArr.length; i++){
-                this.PDCount += childArr[i].calcPDCount();
+                var layerNumber = childArr[i].layerNumber = this.layerNumber + 1;
+                this.PDCount += childArr[i].calcPDCount_setLayersNumbers();
+                lhca[layerNumber] = lhca[layerNumber]+1 || 1;
             }
             return this.PDCount;
         }
-
-
-        //THIS METHOD IS STILL TO BE COMPLETED LATER BY MEHRDAD
-        /*
-        calcChildLayer_headCount(){
-            this.ChildLayer_headCount = 1;
-            var SiblingsArr = this.getParent().getparent().getChildren();
-            for (var i = 0; i < SiblingsArr.length, i++){
-                var sibling = SiblingsArr[i];
-                var childArr = sibling.getChildren();
-                for(var i=0; i < childArr.length; i++){
-                    this.ChildLayer_headCount += (childArr[i].getChildren()).length;
-                }
-            }
-            return this.ChildLayer_headCount;
-        */
 
         //returns the PDCount
         getPDCount(){
@@ -83,42 +67,38 @@
             }
         }
 
-        //THIS METHOD IS STILL WORK IN PROGRESS; YET TO BE COMPLETED LATER BY MEHRDAD
-        //For now it retruns 80 for all nodes.
+        //calculates the thickness of child layer
         calcChildLayer_Thickness(){
-            // FOR MEHRDAD: PASS THESE AS ARGUMENTS: layerCount, parentSize, inRadius
-            var minimumThickness = 80;
-            var aThickness = minimumThickness;
-            // var density = parentSize*inRadius/layerCount;
-            // var aThickness = minimumThickness;
-            // if (density < 5){
-            //     aThickness = 100;
-            // // } else if (density < 2) {
-            // //     aThickness = 80;
-            // // } else if (density < 1.5) {
-            // //     aThickness = 60;
-            // // } else if (density < 2) {
-            // //     aThickness = 40;
-            // }
-        return aThickness;
-    }
+            var childLayer_Thickness = 50;
+            var childLayerPopulation =
+                this.getVisualisation().layerHeadCountArr[this.layerNumber + 1];
+            // console.log(childLayerPopulation);
+            var avgPixelPerNode =
+                this.radialBand.getOutRadius()*2*Math.PI / childLayerPopulation;
+            if (avgPixelPerNode < 2){
+                childLayer_Thickness = 120
+            } else if (avgPixelPerNode < 10) {
+                childLayer_Thickness = 100
+            }
+            return childLayer_Thickness;
+        }
 
         //set children properties: inRadius, thickness, startAngle, size,
         setPropertiesDescendants(){
             var nextStartAngle = this.radialBand.getStartAngle();
-            var children = this.getChildren();
-            var layerThickness =  this.calcChildLayer_Thickness();
-            for(var i=0; i<children.length; i++){
-                var child = children[i];
+            var childrenArr = this.getChildren();
+            var childlayerThickness = this.calcChildLayer_Thickness();
+            for(var i=0; i<childrenArr.length; i++){
+                var child = childrenArr[i];
                 child.radialBand.resetProperties();
-                child.radialBand.setInRadius(this.radialBand.getInRadius() + this.radialBand.getThickness());
-                child.radialBand.setThickness(layerThickness);
+                child.radialBand.setInRadius(this.radialBand.getInRadius() + this.radialBand.getThickness() + 2);
+                child.radialBand.setThickness(childlayerThickness);
                 child.radialBand.setStartAngle(nextStartAngle);
                 var aSize = this.radialBand.getSize()*(child.getPDCount()/this.calcChildLayer_PDCount());
                 child.radialBand.setSize(aSize);
                 nextStartAngle += aSize; //implementation chosen for speed; could've used child.getEndAngle
                 child.setPropertiesDescendants();
-                child.radialBand.setColor(new VIZ2D.Color.fromHSV(Math.random(), 1, 1).getInt());
+                child.radialBand.setColor(new VIZ2D.Color.fromHSV(Math.random(),1,1).getInt());
             };
         }
 
@@ -136,7 +116,8 @@
         //
         constructor(container, tree, options){
             super(container, tree, options);
-            this.centeralizeNode(tree.getRoot());
+            this.maxNodeCount = 2000;
+            this.centeralizeNode(tree.getRoot(), 7);
         }
 
         //
@@ -144,7 +125,10 @@
             return NodeShape;
         }
 
-        //
+        /*
+        The options for recentering on main root, as well as recentering on the
+        parent of a node.
+        */
         __setupOptions(options){
             var This = this;
             options.add(new Options.Button("center").onClick(function(){
@@ -152,23 +136,41 @@
             }));
         }
 
-        //Set a node as a new center and the build visualisation around it
-        centeralizeNode(node){
-            var layers = 10;
+        //Set a node as a new center and then build visualisation around it
+        centeralizeNode(node, layers){
+            var layers = layers;
             //stops rendering all visible nodeshapes
-            var nodeshapeArr = this.getShapesNode();
-            for (var i=nodeshapeArr.length-1; i >= 0; i--){
-                nodeshapeArr[i].remove()
+            var nodeShapeArr = this.getShapes();
+            for (var i=nodeShapeArr.length-1; i >= 0; i--){
+                nodeShapeArr[i].remove()
+            }
+            //reset the array layerHeadCountArr if it exists
+            if (this.layerHeadCountArr != null){
+                this.layerHeadCountArr.length = 0
+            } else{
+                this.layerHeadCountArr = [];
             }
             //build the viusalisation around the new center
             var centerNode = this.createNodeShape(node).add();
+            centerNode.layerNumber = 0;
+            this.layerHeadCountArr[0] = 1;
             centerNode.createDescendants(layers);
-            //as a result PDCount is calculated for the node and all its descendants:
-            centerNode.calcPDCount();
-            //"PDCount" stands for "partial descendants count"
+            //remove the outer layer nodes since that layer is incomplete due to
+            //reaching the maxNodeCount
+            var nodeShapeArr = this.getShapes();
+            var outerLayerDepth = nodeShapeArr[nodeShapeArr.length-1].getDepth();
+            for (var i=nodeShapeArr.length-1; i >= 0; i--){
+                var nodeShape = nodeShapeArr[i];
+                if(nodeShape.getDepth()!=outerLayerDepth)
+                    break;
+                nodeShape.remove();
+            }
+            //set PDCount and layernumber for this nodeshape and its descendants
+            centerNode.calcPDCount_setLayersNumbers();
+            //set the properties of the discendants so they are shown on the screen
             centerNode.setPropertiesDescendants();
         }
-    }
+     }
 
 
     /*
